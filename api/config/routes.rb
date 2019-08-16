0f63a715700718498541a3fc457c2d31d46b7677
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-17 03:26:34 +0700

    change controller name to keyword file

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :upload_file, path: "upload-file", only: [:index, :show]
+      resources :keyword_file, path: "upload-file", only: [:index, :show, :create]
     end
   end
 end
