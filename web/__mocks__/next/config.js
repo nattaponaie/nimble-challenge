aa71eff506e93bbfc96c33b5c0bcc9cc8629c957
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-18 22:34:56 +0700

    remove unused config

--- web/__mocks__/next/config.js
+++ web/__mocks__/next/config.js
@@ -1,10 +1,7 @@
 module.exports = () => ({
   publicRuntimeConfig: {
     ASSET_PREFIX: process.env.ASSET_PREFIX,
-    API_PREFIX: process.env.API_PREFIX,
-    GATEWAY_PREFIX: process.env.GATEWAY_PREFIX,
     GATEWAY_RECAPTCHA_SITE_KEY: process.env.GATEWAY_RECAPTCHA_SITE_KEY,
-    DEV_MODE: process.env.DEV_MODE,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT: process.env.FIREBASE_PROJECT,
