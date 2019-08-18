8065086a04a00b4ddb94bbf961b32bb12b47948e
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-18 17:06:33 +0700

    implement papaParse

---
+++ web/utils/papaParse.js
@@ -0,0 +1,23 @@
+import Papa from 'papaparse';
+import { head } from 'lodash';
+
+const MAX_KEYWORDS = 100;
+
+const parseCsvFile = async (csv) => {
+  return new Promise((resolve, reject) => {
+    Papa.parse(csv, {
+      worker: true,
+      error: reject,
+      complete: (results) => {
+        const { data } = results;
+        if(data.length > MAX_KEYWORDS) reject(`Exceed max keywords (1-${MAX_KEYWORDS})!`);
+        const keywords = data.map((d) => head(d));
+        resolve(keywords);
+      }
+    });
+  });
+};
+
+export {
+  parseCsvFile,
+};
