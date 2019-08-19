a9f4ce24b4dee319565e3e11f6979feb71874907
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-19 23:41:46 +0700

    rename function

--- web/__mocks__/utils/httpHelperMock.js
+++ web/__mocks__/utils/httpHelperMock.js
@@ -1,7 +1,7 @@
 export const mockAccessToken = 'adowqjfro2jt92jg2ig2ihith2it2';
 
-export const urlPathMock = { version: 'v1', path: 'upload-file' };
+export const mockUrlPath = { version: 'v1', path: 'upload-file' };
 
-export const instanceGetMock = () => Promise.resolve({ data: { data: [] } });
+export const mockInstanceGet = () => Promise.resolve({ data: { data: [] } });
 
-export const instancePostMock = () => Promise.resolve({ data: { statusMsg: 'success' } });
+export const mockInstancePost = () => Promise.resolve({ data: { statusMsg: 'success' } });
