8ecf36341c507758eb60b64fa4ba9d2e07ed8b2d
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-23 03:06:03 +0700

    include class to application controller

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  include ParamsIncludable
+  include UserUidAuthenticable
+  include ParamsDeserializer
   skip_before_action :verify_authenticity_token
 end
