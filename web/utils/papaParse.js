695e122d9290a283a1d1a04e0af6d631865561df
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-08-23 15:18:46 +0700

    make parseCsv file to accpet more format

--- web/utils/papaParse.js
+++ web/utils/papaParse.js
@@ -3,15 +3,39 @@ import Papa from 'papaparse';
 
 const MAX_KEYWORDS = 100;
 
+export const isExceedMaximum = (dataLength) => {
+  if(dataLength > MAX_KEYWORDS) return true;
+  return false;
+};
+
+export const mapKeyword = (keyword) => {
+  if(keyword.length === 1) {
+    // when csv file is in comma format
+    // example: rails,ruby,developer job
+    return head(keyword);
+  }
+  else {
+    // when csv file is column format
+    // example:
+    // rails
+    // ruby
+    // developer
+    // job
+    return keyword.map((key) => head(key));
+  }
+};
+
 export const parseCsvFile = async (csv) => {
   return new Promise((resolve, reject) => {
     Papa.parse(csv, {
       worker: true,
       error: reject,
+      comments: '#',
+      skipEmptyLines: true,
       complete: (results) => {
         const { data } = results;
-        if(data.length > MAX_KEYWORDS) reject(`Exceed max keywords (1-${MAX_KEYWORDS})!`);
-        const keywords = data.map((d) => head(d));
+        if (isExceedMaximum(data.length)) reject(`Exceed max keywords (1-${MAX_KEYWORDS})!`);
+        const keywords = mapKeyword(data);
         resolve(keywords);
       },
     });
